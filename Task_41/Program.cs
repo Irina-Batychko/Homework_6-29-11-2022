d44906ea118b6d3602ea9545203a691a29de8d86
IRINA BATYCHKO <mironenkoi@yandex.ru>
2022-12-05 22:42:27 +0300

    added tasks

---
+++ Task_41/Program.cs
@@ -0,0 +1,27 @@
+﻿/* **Задача 41:**
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223 -> 3  */
+
+string[] input()
+{
+    Console.WriteLine("Введите несколько целых чисел через запятую: ");
+    string? strA = Console.ReadLine();
+    string[] strB = strA.Split(",");
+    return strB;
+}
+string[] array = input();
+
+double[] strA1 = Array.ConvertAll(array, double.Parse);
+int numbers = array.Length;
+Console.WriteLine("\nКоличество чисел: " + numbers);
+
+int countZero = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (strA1[i] > 0)
+    {
+        countZero++;
+    }
+}
+Console.WriteLine("Количество чисел больше нуля: " + countZero);
---
+++ Task_43/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+double b1 = Read("Введите значение b1: ");
+double k1 = Read("Введите значение k1: ");
+double b2 = Read("Введите значение b2: ");
+double k2 = Read("Введите значение k2: ");
+
+if (k1 == k2)
+{
+    Console.WriteLine("Две прямые параллельны!");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Две прямые пересекутся в X:{x}, Y:{y}");
+}
+double Read(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
